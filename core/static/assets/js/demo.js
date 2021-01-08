53af9714781a8c820039307da86ed938f4c0ab87
rohola <hilbert.cantor@gmail.com>
2021-01-08 15:45:09 -0700

    update names

--- core/static/assets/js/demo.js
+++ core/static/assets/js/demo.js
@@ -89,18 +89,18 @@ Circles.create({
 });
 
 //Notify
-$.notify({
-	icon: 'flaticon-alarm-1',
-	title: 'MyCogito',
-	message: 'Updated!',
-},{
-	type: 'info',
-	placement: {
-		from: "bottom",
-		align: "right"
-	},
-	time: 1000,
-});
+// $.notify({
+// 	icon: 'flaticon-alarm-1',
+// 	title: 'MyCogito',
+// 	message: 'Updated!',
+// },{
+// 	type: 'info',
+// 	placement: {
+// 		from: "bottom",
+// 		align: "right"
+// 	},
+// 	time: 1000,
+// });
 
 // JQVmap
 $('#map-example').vectorMap(
