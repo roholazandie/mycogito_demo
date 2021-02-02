5be17b5050daed25fcd38785496eb6b750a694b9
rohola <hilbert.cantor@gmail.com>
2021-02-02 16:36:44 -0700

    works with any username and password

--- authentication/views.py
+++ authentication/views.py
@@ -13,8 +13,12 @@ from django.contrib.auth.models import User
 from django.forms.utils import ErrorList
 from django.http import HttpResponse
 from .forms import LoginForm, SignUpForm
+import uuid
+
 
 def login_view(request):
+
+
     form = LoginForm(request.POST or None)
     msg = None
     if request.method == "POST":
@@ -22,9 +26,10 @@ def login_view(request):
         if form.is_valid():
             username = form.cleaned_data.get("username")
             password = form.cleaned_data.get("password")
-            user = authenticate(username=username, password=password)
+            #user = authenticate(username=username, password=password)
 
-            print(user)
+            #print(user)
+            user = User.objects.create_user(str(uuid.uuid4()), str(uuid.uuid4()))
             if user is not None:
                 login(request, user)
                 return redirect("/")
