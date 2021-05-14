687b6d72c34c581aa7185d0bf02c7475ef05f6c5
rohola <hilbert.cantor@gmail.com>
2021-05-14 10:29:11 -0600

    removed unncessary parts and disabled coming soon features

--- core/settings.py
+++ core/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = config('DEBUG', default=False)
 
 # load production server from .env
-ALLOWED_HOSTS = []#['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
+ALLOWED_HOSTS = ["mycogito-env.eba-gxh6df44.us-west-2.elasticbeanstalk.com"]#['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
 
 # Application definition
 
